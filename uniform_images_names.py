328318f143668088a1177bc16df3251e2f2c7380
MRajaeiM <mrajaeim.pro@gmail.com>
2023-02-04 11:48:47 +0330

    [fix] rename dataset images

--- uniform_images_names.py
+++ uniform_images_names.py
@@ -2,8 +2,8 @@ import os
 
 for file in os.listdir('./dataset/'):
     path = f"./dataset/{file}"
-    image_number, ext = file.split(".")
+    image_number, version = file[0:4], file[5:7]
     deg = (int(image_number) * 360) / 250
-    new_path = f"./dataset/{image_number}_v{0}_deg_{deg:0.2f}.{ext}"
+    new_path = f"./dataset/{image_number:04}_{version}_deg_{deg:06.2f}.jpg"
     # print(new_path)
     os.rename(path, new_path)
